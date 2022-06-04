32aff2b4793058d785b89723e6a0d0c51bcdac9d
Aaron J Yoder <aaronjyoder@gmail.com>
2022-06-04 18:55:11 -0400

    Add support for decoding legacy palletes

--- src/main/java/com/riskrieg/codec/decode/RkpDecoder.java
+++ src/main/java/com/riskrieg/codec/decode/RkpDecoder.java
@@ -20,6 +20,7 @@ package com.riskrieg.codec.decode;
 
 import com.riskrieg.codec.internal.json.JsonHelper;
 import com.riskrieg.palette.RkpPalette;
+import com.riskrieg.palette.legacy.LegacyPalette;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -32,22 +33,61 @@ public final class RkpDecoder implements Decoder<RkpPalette> {
   @Override
   public RkpPalette decode(Path path) throws IOException {
     Objects.requireNonNull(path);
-    return JsonHelper.read(path, RkpPalette.class);
+    try {
+      return JsonHelper.read(path, RkpPalette.class);
+    } catch (Exception e) {
+      return decodeLegacy(path);
+    }
+  }
+
+  private RkpPalette decodeLegacy(Path path) throws IOException {
+    try {
+      return JsonHelper.read(path, LegacyPalette.class).toRkpPalette();
+    } catch (NullPointerException e) {
+      return null;
+    }
   }
 
   @Override
   public RkpPalette decode(URL url) throws IOException {
     Objects.requireNonNull(url);
+    try {
+      try (InputStream inputStream = url.openStream()) {
+        final byte[] data = inputStream.readAllBytes();
+        return JsonHelper.read(new String(data, StandardCharsets.UTF_8), RkpPalette.class);
+      }
+    } catch (Exception e) {
+      return decodeLegacy(url);
+    }
+  }
+
+  private RkpPalette decodeLegacy(URL url) throws IOException {
     try (InputStream inputStream = url.openStream()) {
       final byte[] data = inputStream.readAllBytes();
-      return JsonHelper.read(new String(data, StandardCharsets.UTF_8), RkpPalette.class);
+      LegacyPalette legacy = JsonHelper.read(new String(data, StandardCharsets.UTF_8), LegacyPalette.class);
+      return legacy.toRkpPalette();
+    } catch (NullPointerException e) {
+      return null;
     }
   }
 
   @Override
   public RkpPalette decode(byte[] data) throws IOException {
     Objects.requireNonNull(data);
-    return JsonHelper.read(new String(data, StandardCharsets.UTF_8), RkpPalette.class);
+    try {
+      return JsonHelper.read(new String(data, StandardCharsets.UTF_8), RkpPalette.class);
+    } catch (Exception e) {
+      return decodeLegacy(data);
+    }
+  }
+
+  private RkpPalette decodeLegacy(byte[] data) throws IOException {
+    try {
+      LegacyPalette legacy = JsonHelper.read(new String(data, StandardCharsets.UTF_8), LegacyPalette.class);
+      return legacy.toRkpPalette();
+    } catch (NullPointerException e) {
+      return null;
+    }
   }
 
 }
